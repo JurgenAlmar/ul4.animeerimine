09a3a7e14478f941dcda0579cb52f1150939164c
Jürgen <jurgen.almar@vikk.ee>
2025-03-19 01:07:50 -0700

    viimane vist

--- ul4..py
+++ ul4..py
@@ -11,42 +11,62 @@ bg = pygame.image.load("bg_rally.jpg")
 f1_sinine = pygame.image.load("f1_blue.png")
 f1_punane = pygame.image.load("f1_red.png")
 
-# Siniste autode algpositsioonid ja kiirus (veel aeglasemad)
 sinised_autod = [
-    {"x": 150, "y": -f1_sinine.get_height(), "kiirus": 0.1},  # Väga aeglane auto
-    {"x": 250, "y": -f1_sinine.get_height(), "kiirus": 0.15},  # Väga aeglane auto
-    {"x": 350, "y": -f1_sinine.get_height(), "kiirus": 0.12}   # Väga aeglane auto
+    {"x": 150, "y": -f1_sinine.get_height(), "kiirus": 0.1},
+    {"x": 250, "y": -f1_sinine.get_height(), "kiirus": 0.15},
+    {"x": 350, "y": -f1_sinine.get_height(), "kiirus": 0.12}
 ]
 
-# Punase auto algpositsioon (ekraani all keskel)
 posX_punane = (ekraani_laius - f1_punane.get_width()) // 2
-posY_punane = ekraani_korgus - f1_punane.get_height()  # Punane auto ekraani all
+posY_punane = ekraani_korgus - f1_punane.get_height()
+
+skoor = 0
+font = pygame.font.Font(None, 36)
+
+punase_auto_kiirus = 0.5  # Muudetud liikumiskiirus aeglasemaks
 
 gameover = False
 while not gameover:
-    # mängu sulgemine ristist
     events = pygame.event.get()
     for i in events:
         if i.type == pygame.QUIT:
             sys.exit()
 
-    # Tausta värskendamine
+    keys = pygame.key.get_pressed()
+
+    if keys[pygame.K_UP]:
+        posY_punane -= punase_auto_kiirus
+    if keys[pygame.K_DOWN]:
+        posY_punane += punase_auto_kiirus
+    if keys[pygame.K_LEFT]:
+        posX_punane -= punase_auto_kiirus
+    if keys[pygame.K_RIGHT]:
+        posX_punane += punase_auto_kiirus
+
+    if posX_punane < 0:
+        posX_punane = 0
+    if posX_punane > ekraani_laius - f1_punane.get_width():
+        posX_punane = ekraani_laius - f1_punane.get_width()
+    if posY_punane < 0:
+        posY_punane = 0
+    if posY_punane > ekraani_korgus - f1_punane.get_height():
+        posY_punane = ekraani_korgus - f1_punane.get_height()
+
     ekraan.fill((0, 0, 0))
     ekraan.blit(bg, (0, 0))
 
-    # Liiguta iga sinine auto (veel aeglasemalt)
     for auto in sinised_autod:
         auto["y"] += auto["kiirus"]
 
-        # Kui auto jõuab ekraani allserva, liigu tagasi üles
         if auto["y"] > ekraani_korgus:
             auto["y"] = -f1_sinine.get_height()
+            skoor += 10
 
-        # Joonista auto ekraanile
         ekraan.blit(f1_sinine, (auto["x"], auto["y"]))
 
-    # Joonista punane auto, mis jääb ekraani all keskele
     ekraan.blit(f1_punane, (posX_punane, posY_punane))
 
-    # Ekraani värskendamine
+    skoor_teksts = font.render("Skoor: " + str(skoor), True, (255, 255, 255))
+    ekraan.blit(skoor_teksts, (10, 10))
+
     pygame.display.update()
